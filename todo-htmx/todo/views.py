7e1807094b314bf7c0509945da1ea35dda1d0823
Vr3n <vickyspatel@gmail.com>
2021-12-06 20:38:24 +0530

    Logout Login and Tasks spa functionality

--- todo-htmx/todo/views.py
+++ todo-htmx/todo/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, "pages/home.html")
+
+
+def auth(request):
+    return render(request, "components/auth.html")
